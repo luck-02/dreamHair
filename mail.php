93c5a4d4a7630c3abc04bfb09d4f1fced2a51f8e
luck-02 <34078241+luck-02@users.noreply.github.com>
2022-09-11 00:51:02 +0200

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,55 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$sujet = $_POST['sujet'];
+$message = $_POST['message'];
+
+$mailheader = "De: ".$name."<".$email.">\r\n";
+$recipient = "luck.ndzamba@gmail.com";
+
+mail($recipient, $sujet, $message, $mailheader) or die("Error!")
+
+echo'
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dream Hair</title>
+  <link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+
+  <div class="contact">
+    <h1> Merci ! Je vous répondrais dès que possible</h1>
+    <p class="back">Retour à <a href="index.html">Accueil</a>.</p>
+  </div>
+
+  <div class="cursor-outer"></div>
+  <div class="cursor-inner"></div>
+
+  <script>
+    let cursor = document.querySelector(".cursor-inner");
+    let cursor2 = document.querySelector(".cursor-outer");
+
+    document.addEventListener("mousemove", e=>{
+      // console.log(e);
+      cursor.style.top = e.clientY + "px";
+      cursor.style.left = e.clientX + "px";
+
+      cursor2.style.top = e.clientY + "px";
+      cursor2.style.left = e.clientX + "px";
+    })
+  </script>
+
+</body>
+</html>
+
+
+';
+
+
+
+ ?>
